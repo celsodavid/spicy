16508c460c9477bee5c2bd6123bb644c5f306463
paulobaronista <paulobaronista@gmail.com>
2014-01-08 17:14:25 -0200

    Novas Imagens e paginas

--- solaris/application/views/content/resort_view.php
+++ solaris/application/views/content/resort_view.php
@@ -1,2 +1,92 @@
-Eu sou o Resort 
-<br>
+<section id="resort">
+
+	<aside>
+		<h2>resort</h2>
+		<p class="tt_medio">prepare-se para se divertir. o resort já está pronto</p>
+		<p class="tt_small">antes que sua casa fique pronta, você poderá aproveitar o complexo de lazer.<br/>
+		e estamos falando apenas de um dos diferenciais de solaris. seu projeto incorpora um conjunto de conceitos 
+		inéditos na região para garantir que sua diversão será completa,<br/> como, por exemplo os monitores que irão 
+		coordenar brincadeiras, gincanas, passeios, atividades físicas e  esportivas.</p>
+
+		<figure>
+			<img src="<?php echo base_url('assets');?>/img/icon_resort.png">
+			<figcaption>clique nas imagens que possuem esse icone, para conhecer melhor o local.</figcaption>
+		</figure>
+
+	</aside>
+
+	<article>
+
+		<div id="planta_resort">
+
+			<figure>
+			  <img src="<?php echo base_url('assets');?>/img/planta_resort.jpg">
+		    </figure>
+
+		    <span id="icones_resort">
+
+		    	<figure id="ico01">
+			      <a href=""><img src="<?php echo base_url('assets');?>/img/icon_resort.png"></a>
+		        </figure>
+
+		        <figure id="ico02">
+			      <a href=""><img src="<?php echo base_url('assets');?>/img/icon_resort.png"></a>
+		        </figure>
+
+		        <figure id="ico03">
+			      <a href=""><img src="<?php echo base_url('assets');?>/img/icon_resort.png"></a>
+		        </figure>
+
+		        <figure id="ico04">
+			      <a href=""><img src="<?php echo base_url('assets');?>/img/icon_resort.png"></a>
+		        </figure>
+
+		        <figure id="ico05">
+			      <a href=""><img src="<?php echo base_url('assets');?>/img/icon_resort.png"></a>
+		        </figure>
+
+		        <figure id="ico06">
+			      <a href=""><img src="<?php echo base_url('assets');?>/img/icon_resort.png"></a>
+		        </figure>
+
+		        <figure id="ico07">
+			      <a href=""><img src="<?php echo base_url('assets');?>/img/icon_resort.png"></a>
+		        </figure>
+
+		        <figure id="ico08">
+			      <a href=""><img src="<?php echo base_url('assets');?>/img/icon_resort.png"></a>
+		        </figure>
+
+		        <figure id="ico09">
+			      <a href=""><img src="<?php echo base_url('assets');?>/img/icon_resort.png"></a>
+		        </figure>
+
+		        <figure id="ico10">
+			      <a href=""><img src="<?php echo base_url('assets');?>/img/icon_resort.png"></a>
+		        </figure>
+
+		        <figure id="ico11">
+			      <a href=""><img src="<?php echo base_url('assets');?>/img/icon_resort.png"></a>
+		        </figure>
+
+		        <figure id="ico12">
+			      <a href=""><img src="<?php echo base_url('assets');?>/img/icon_resort.png"></a>
+		        </figure>
+
+		        <figure id="ico13">
+			      <a href=""><img src="<?php echo base_url('assets');?>/img/icon_resort.png"></a>
+		        </figure>
+
+		        <figure id="ico14">
+			      <a href=""><img src="<?php echo base_url('assets');?>/img/icon_resort.png"></a>
+		        </figure>
+		        
+		    </span>
+
+		</div>
+		
+		
+		
+	</article>
+
+</section>
--- solaris/application/views/header.php
+++ solaris/application/views/header.php
@@ -1,1 +1,18 @@
-Eu sou o header<br>
+<header>
+	<section>
+
+		<h1>
+			<a href="#" title="Solaris Residencial e Resort" alt="Solaris Residencial e Resort">Solaris Residencial e Resort</a>
+		</h1>
+
+		<nav>
+			<ul>
+				<li><a href="" title="Residencial">Residencial</a></li>
+				<li><a href="" title="Resort">Resort</a></li>
+				<li><a href="" title="Localização">Localização</a></li>
+				<li><a href="" title="Contato">Contato</a></li>
+			</ul>
+		</nav>
+
+	</section>
+</header>
