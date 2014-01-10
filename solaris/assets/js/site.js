17b889fb28689528204a25299c89853e6803aa40
dereckleme <dereckleme@globo.com>
2014-01-10 14:58:08 -0200

    dereck

--- solaris/assets/js/site.js
+++ solaris/assets/js/site.js
@@ -1,9 +1,17 @@
 $(function(){
+		var width = $(window).width();
+		if(width > 1345)
+		{
+		var tamanhoFonte = width*0.35/100;
+		}
+		$("h3 a").css("fontSize", tamanhoFonte+"em");
 	window.onresize = function(event) {
 		var width = $(window).width();
+		if(width > 1345)
+		{
 		var tamanhoFonte = width*0.3/100;
 		$("h3 a").css("fontSize", tamanhoFonte+"em");
-		
+		}
 	}
 	
 });
