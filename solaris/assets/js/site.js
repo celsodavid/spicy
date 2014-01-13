b4f5c69990a0a109020207a831141e4bf75bc19f
dereckleme <dereckleme@globo.com>
2014-01-13 12:12:20 -0200

    dereck

--- solaris/assets/js/site.js
+++ solaris/assets/js/site.js
@@ -1,5 +1,6 @@
 $(function(){
 		var width = $(window).width();
+		
 		if(width > 1345)
 		{
 		var tamanhoFonte = width*0.45/100;
@@ -13,6 +14,30 @@ $(function(){
 		$("h3 a").css("fontSize", tamanhoFonte+"em");
 		}
 	}
+	var positionTop = false;
+	var positionLeft = false;
+	$(".activePopUp").hover(function(){
+		var topMargin = $(this).css("marginTop").replace('px', '');
+		var leftMargin = $(this).css("marginLeft").replace('px', '');
+		//tamanho do container
+		var altura = $("#planta_resort").height();
+		var largura = $("#planta_resort").width();
+		if(positionTop != topMargin || positionLeft != leftMargin)
+		{
+			$("#box-shadow").fadeOut(100,function(){
+				$(this).css({
+				marginTop:(((topMargin)/altura*100))+"%",
+				marginLeft:(((leftMargin-123)/largura*100))+"%"
+				});
+				$("#box-shadow").fadeIn();
+				positionTop = topMargin;
+				positionLeft = leftMargin;
+			});
+		}
+	},function(){
+	
+		});
+	
 	$(".checkBg").hover(function(){
 		$(".fundoPreto",this).fadeIn(200);
 		$("a",this).animate({
