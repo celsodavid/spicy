1c1cf93bc44b469d0927446fc3663974a68acbfd
paulobaronista <paulobaronista@gmail.com>
2014-01-27 10:56:19 -0200

    Atualização resort_view.php

--- solaris/application/views/content/resort_view.php
+++ solaris/application/views/content/resort_view.php
@@ -25,17 +25,13 @@
 
 		    <span id="icones_resort">
 
-		    	<figure id="ico01">
+		        <figure id="ico01">
 			      <a href="" alt="" title="">
 			      	<div class="box-shadow ico01">
-			    		<img class="imagem_resort" src="<?php echo base_url('assets');?>/img/centrohipico.jpg">
+			    		<img class="imagem_resort" src="<?php echo base_url('assets');?>/img/semimagem.jpg">
 			    		<img src="<?php echo base_url('assets');?>/img/sobra_box_resort.png">
-			    		<p class="tt_box-shadow">centro hípico</p>
-			    		<p class="txt_box-shadow">Tudo para cavaleiros amadores e 
-							profissionais: instrutores, veterinários.
-							Circuito para enduro equestre e um 
-							haras com 52 baias para deixar seu 
-							cavalo e os do empreendimento.</p>
+			    		<p class="tt_box-shadow">Mini fazenda</p>
+			    		<p class="txt_box-shadow">Tudo para seus filhos interagirem com uma comunidade rural e experimentarem a verdadeira vida no campo.</p>
 	                    <img class="dt_box" src="<?php echo base_url('assets');?>/img/detalhe_sobra_box_resort.png">
 		    	    </div>
 			      	<img src="<?php echo base_url('assets');?>/img/icon_resort.png">
@@ -45,14 +41,10 @@
 		        <figure id="ico02">
 			      <a href="" alt="" title="">
 			      	<div class="box-shadow ico02">
-			    		<img class="imagem_resort" src="<?php echo base_url('assets');?>/img/centrohipico.jpg">
+			    		<img class="imagem_resort" src="<?php echo base_url('assets');?>/img/semimagem.jpg">
 			    		<img src="<?php echo base_url('assets');?>/img/sobra_box_resort.png">
-			    		<p class="tt_box-shadow">centro hípico</p>
-			    		<p class="txt_box-shadow">Tudo para cavaleiros amadores e 
-							profissionais: instrutores, veterinários.
-							Circuito para enduro equestre e um 
-							haras com 52 baias para deixar seu 
-							cavalo e os do empreendimento.</p>
+			    		<p class="tt_box-shadow">Mini fazenda</p>
+			    		<p class="txt_box-shadow">Tudo para seus filhos interagirem com uma comunidade rural e experimentarem a verdadeira vida no campo.</p>
 	                    <img class="dt_box" src="<?php echo base_url('assets');?>/img/detalhe_sobra_box_resort.png">
 		    	    </div>
 			      	<img src="<?php echo base_url('assets');?>/img/icon_resort.png">
@@ -62,14 +54,10 @@
 		        <figure id="ico03">
 			      <a href="" alt="" title="">
 			      	<div class="box-shadow ico03">
-			    		<img class="imagem_resort" src="<?php echo base_url('assets');?>/img/centrohipico.jpg">
+			    		<img class="imagem_resort" src="<?php echo base_url('assets');?>/img/piscina.jpg">
 			    		<img src="<?php echo base_url('assets');?>/img/sobra_box_resort.png">
-			    		<p class="tt_box-shadow">centro hípico</p>
-			    		<p class="txt_box-shadow">Tudo para cavaleiros amadores e 
-							profissionais: instrutores, veterinários.
-							Circuito para enduro equestre e um 
-							haras com 52 baias para deixar seu 
-							cavalo e os do empreendimento.</p>
+			    		<p class="tt_box-shadow">piscinas</p>
+			    		<p class="txt_box-shadow">Um complexo com piscinas e inúmeras atividades para adultos e crianças.</p>
 	                    <img class="dt_box" src="<?php echo base_url('assets');?>/img/detalhe_sobra_box_resort.png">
 		    	    </div>
 			      	<img src="<?php echo base_url('assets');?>/img/icon_resort.png">
@@ -79,14 +67,10 @@
 		        <figure id="ico04">
 			      <a href="" alt="" title="">
 			      	<div class="box-shadow ico04">
-			    		<img class="imagem_resort" src="<?php echo base_url('assets');?>/img/centrohipico.jpg">
+			    		<img class="imagem_resort" src="<?php echo base_url('assets');?>/img/semimagem.jpg">
 			    		<img src="<?php echo base_url('assets');?>/img/sobra_box_resort.png">
-			    		<p class="tt_box-shadow">centro hípico</p>
-			    		<p class="txt_box-shadow">Tudo para cavaleiros amadores e 
-							profissionais: instrutores, veterinários.
-							Circuito para enduro equestre e um 
-							haras com 52 baias para deixar seu 
-							cavalo e os do empreendimento.</p>
+			    		<p class="tt_box-shadow">lago com praia</p>
+			    		<p class="txt_box-shadow">Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica.</p>
 	                    <img class="dt_box" src="<?php echo base_url('assets');?>/img/detalhe_sobra_box_resort.png">
 		    	    </div>
 			      	<img src="<?php echo base_url('assets');?>/img/icon_resort.png">
@@ -96,31 +80,23 @@
 		        <figure id="ico05">
 			      <a href="" alt="" title="">
 			      	<div class="box-shadow ico05">
-			    		<img class="imagem_resort" src="<?php echo base_url('assets');?>/img/centrohipico.jpg">
+			    		<img class="imagem_resort" src="<?php echo base_url('assets');?>/img/semimagem.jpg">
 			    		<img src="<?php echo base_url('assets');?>/img/sobra_box_resort.png">
-			    		<p class="tt_box-shadow">centro hípico</p>
-			    		<p class="txt_box-shadow">Tudo para cavaleiros amadores e 
-							profissionais: instrutores, veterinários.
-							Circuito para enduro equestre e um 
-							haras com 52 baias para deixar seu 
-							cavalo e os do empreendimento.</p>
+			    		<p class="tt_box-shadow">haras</p>
+			    		<p class="txt_box-shadow">Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica.</p>
 	                    <img class="dt_box" src="<?php echo base_url('assets');?>/img/detalhe_sobra_box_resort.png">
 		    	    </div>
 			      	<img src="<?php echo base_url('assets');?>/img/icon_resort.png">
 			      </a>
 		        </figure>
 
-		        <figure id="ico06">
+		         <figure id="ico06">
 			      <a href="" alt="" title="">
 			      	<div class="box-shadow ico06">
-			    		<img class="imagem_resort" src="<?php echo base_url('assets');?>/img/centrohipico.jpg">
+			    		<img class="imagem_resort" src="<?php echo base_url('assets');?>/img/semimagem.jpg">
 			    		<img src="<?php echo base_url('assets');?>/img/sobra_box_resort.png">
-			    		<p class="tt_box-shadow">centro hípico</p>
-			    		<p class="txt_box-shadow">Tudo para cavaleiros amadores e 
-							profissionais: instrutores, veterinários.
-							Circuito para enduro equestre e um 
-							haras com 52 baias para deixar seu 
-							cavalo e os do empreendimento.</p>
+			    		<p class="tt_box-shadow">trilhas</p>
+			    		<p class="txt_box-shadow">Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica.</p>
 	                    <img class="dt_box" src="<?php echo base_url('assets');?>/img/detalhe_sobra_box_resort.png">
 		    	    </div>
 			      	<img src="<?php echo base_url('assets');?>/img/icon_resort.png">
@@ -130,14 +106,10 @@
 		        <figure id="ico07">
 			      <a href="" alt="" title="">
 			      	<div class="box-shadow ico07">
-			    		<img class="imagem_resort" src="<?php echo base_url('assets');?>/img/centrohipico.jpg">
+			    		<img class="imagem_resort" src="<?php echo base_url('assets');?>/img/ponte.jpg">
 			    		<img src="<?php echo base_url('assets');?>/img/sobra_box_resort.png">
-			    		<p class="tt_box-shadow">centro hípico</p>
-			    		<p class="txt_box-shadow">Tudo para cavaleiros amadores e 
-							profissionais: instrutores, veterinários.
-							Circuito para enduro equestre e um 
-							haras com 52 baias para deixar seu 
-							cavalo e os do empreendimento.</p>
+			    		<p class="tt_box-shadow">rio sorocaba (ponte)</p>
+			    		<p class="txt_box-shadow">Uma ponte sobre o Rio Sorocaba para contemplar a natureza e o movimento das águas.</p>
 	                    <img class="dt_box" src="<?php echo base_url('assets');?>/img/detalhe_sobra_box_resort.png">
 		    	    </div>
 			      	<img src="<?php echo base_url('assets');?>/img/icon_resort.png">
@@ -147,14 +119,10 @@
 		        <figure id="ico08">
 			      <a href="" alt="" title="">
 			      	<div class="box-shadow ico08">
-			    		<img class="imagem_resort" src="<?php echo base_url('assets');?>/img/centrohipico.jpg">
+			    		<img class="imagem_resort" src="<?php echo base_url('assets');?>/img/quiosque.jpg">
 			    		<img src="<?php echo base_url('assets');?>/img/sobra_box_resort.png">
-			    		<p class="tt_box-shadow">centro hípico</p>
-			    		<p class="txt_box-shadow">Tudo para cavaleiros amadores e 
-							profissionais: instrutores, veterinários.
-							Circuito para enduro equestre e um 
-							haras com 52 baias para deixar seu 
-							cavalo e os do empreendimento.</p>
+			    		<p class="tt_box-shadow">quiosque no lago</p>
+			    		<p class="txt_box-shadow">Um pier para quem quer pescar e praticar esportes aquáticos.</p>
 	                    <img class="dt_box" src="<?php echo base_url('assets');?>/img/detalhe_sobra_box_resort.png">
 		    	    </div>
 			      	<img src="<?php echo base_url('assets');?>/img/icon_resort.png">
@@ -164,14 +132,10 @@
 		        <figure id="ico09">
 			      <a href="" alt="" title="">
 			      	<div class="box-shadow ico09">
-			    		<img class="imagem_resort" src="<?php echo base_url('assets');?>/img/centrohipico.jpg">
+			    		<img class="imagem_resort" src="<?php echo base_url('assets');?>/img/quadra.jpg">
 			    		<img src="<?php echo base_url('assets');?>/img/sobra_box_resort.png">
-			    		<p class="tt_box-shadow">centro hípico</p>
-			    		<p class="txt_box-shadow">Tudo para cavaleiros amadores e 
-							profissionais: instrutores, veterinários.
-							Circuito para enduro equestre e um 
-							haras com 52 baias para deixar seu 
-							cavalo e os do empreendimento.</p>
+			    		<p class="tt_box-shadow">quadra poliesportiva</p>
+			    		<p class="txt_box-shadow">Futebol, vôlei, handball e basquete.</p>
 	                    <img class="dt_box" src="<?php echo base_url('assets');?>/img/detalhe_sobra_box_resort.png">
 		    	    </div>
 			      	<img src="<?php echo base_url('assets');?>/img/icon_resort.png">
@@ -181,14 +145,10 @@
 		        <figure id="ico10">
 			      <a href="" alt="" title="">
 			      	<div class="box-shadow ico10">
-			    		<img class="imagem_resort" src="<?php echo base_url('assets');?>/img/centrohipico.jpg">
+			    		<img class="imagem_resort" src="<?php echo base_url('assets');?>/img/quadra.jpg">
 			    		<img src="<?php echo base_url('assets');?>/img/sobra_box_resort.png">
-			    		<p class="tt_box-shadow">centro hípico</p>
-			    		<p class="txt_box-shadow">Tudo para cavaleiros amadores e 
-							profissionais: instrutores, veterinários.
-							Circuito para enduro equestre e um 
-							haras com 52 baias para deixar seu 
-							cavalo e os do empreendimento.</p>
+			    		<p class="tt_box-shadow">quadra poliesportiva</p>
+			    		<p class="txt_box-shadow">Futebol, vôlei, handball e basquete.</p>
 	                    <img class="dt_box" src="<?php echo base_url('assets');?>/img/detalhe_sobra_box_resort.png">
 		    	    </div>
 			      	<img src="<?php echo base_url('assets');?>/img/icon_resort.png">
@@ -198,14 +158,10 @@
 		        <figure id="ico11">
 			      <a href="" alt="" title="">
 			      	<div class="box-shadow ico11">
-			    		<img class="imagem_resort" src="<?php echo base_url('assets');?>/img/centrohipico.jpg">
+			    		<img class="imagem_resort" src="<?php echo base_url('assets');?>/img/semimagem.jpg">
 			    		<img src="<?php echo base_url('assets');?>/img/sobra_box_resort.png">
-			    		<p class="tt_box-shadow">centro hípico</p>
-			    		<p class="txt_box-shadow">Tudo para cavaleiros amadores e 
-							profissionais: instrutores, veterinários.
-							Circuito para enduro equestre e um 
-							haras com 52 baias para deixar seu 
-							cavalo e os do empreendimento.</p>
+			    		<p class="tt_box-shadow">quadras de tênis</p>
+			    		<p class="txt_box-shadow">Quadras e professores para quem quer começar ou já sabe jogar</p>
 	                    <img class="dt_box" src="<?php echo base_url('assets');?>/img/detalhe_sobra_box_resort.png">
 		    	    </div>
 			      	<img src="<?php echo base_url('assets');?>/img/icon_resort.png">
@@ -215,15 +171,11 @@
 		        <figure id="ico12">
 			      <a href="" alt="" title="">
 			      	<div class="box-shadow ico12">
-			    		<img class="imagem_resort" src="<?php echo base_url('assets');?>/img/centrohipico.jpg">
+			    		<img class="imagem_resort" src="<?php echo base_url('assets');?>/img/semimagem.jpg">
 			    		<img src="<?php echo base_url('assets');?>/img/sobra_box_resort.png">
-			    		<p class="tt_box-shadow">centro hípico</p>
-			    		<p class="txt_box-shadow">Tudo para cavaleiros amadores e 
-							profissionais: instrutores, veterinários.
-							Circuito para enduro equestre e um 
-							haras com 52 baias para deixar seu 
-							cavalo e os do empreendimento.</p>
-	                    <img class="dt_box-ico12" src="<?php echo base_url('assets');?>/img/detalhe_sobra_box_resort2.png">
+			    		<p class="tt_box-shadow">campo de futebol com gramado</p>
+			    		<p class="txt_box-shadow">Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica.</p>
+	                    <img class="dt_box" src="<?php echo base_url('assets');?>/img/detalhe_sobra_box_resort.png">
 		    	    </div>
 			      	<img src="<?php echo base_url('assets');?>/img/icon_resort.png">
 			      </a>
@@ -232,14 +184,10 @@
 		        <figure id="ico13">
 			      <a href="" alt="" title="">
 			      	<div class="box-shadow ico13">
-			    		<img class="imagem_resort" src="<?php echo base_url('assets');?>/img/centrohipico.jpg">
+			    		<img class="imagem_resort" src="<?php echo base_url('assets');?>/img/semimagem.jpg">
 			    		<img src="<?php echo base_url('assets');?>/img/sobra_box_resort.png">
-			    		<p class="tt_box-shadow">centro hípico</p>
-			    		<p class="txt_box-shadow">Tudo para cavaleiros amadores e 
-							profissionais: instrutores, veterinários.
-							Circuito para enduro equestre e um 
-							haras com 52 baias para deixar seu 
-							cavalo e os do empreendimento.</p>
+			    		<p class="tt_box-shadow">academia</p>
+			    		<p class="txt_box-shadow">Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica.</p>
 	                    <img class="dt_box" src="<?php echo base_url('assets');?>/img/detalhe_sobra_box_resort.png">
 		    	    </div>
 			      	<img src="<?php echo base_url('assets');?>/img/icon_resort.png">
@@ -249,14 +197,75 @@
 		        <figure id="ico14">
 			      <a href="" alt="" title="">
 			      	<div class="box-shadow ico14">
-			    		<img class="imagem_resort" src="<?php echo base_url('assets');?>/img/centrohipico.jpg">
+			    		<img class="imagem_resort" src="<?php echo base_url('assets');?>/img/semimagem.jpg">
 			    		<img src="<?php echo base_url('assets');?>/img/sobra_box_resort.png">
-			    		<p class="tt_box-shadow">centro hípico</p>
-			    		<p class="txt_box-shadow">Tudo para cavaleiros amadores e 
-							profissionais: instrutores, veterinários.
-							Circuito para enduro equestre e um 
-							haras com 52 baias para deixar seu 
-							cavalo e os do empreendimento.</p>
+			    		<p class="tt_box-shadow">baias</p>
+			    		<p class="txt_box-shadow">Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica.</p>
+	                    <img class="dt_box" src="<?php echo base_url('assets');?>/img/detalhe_sobra_box_resort.png">
+		    	    </div>
+			      	<img src="<?php echo base_url('assets');?>/img/icon_resort.png">
+			      </a>
+		        </figure>
+
+		        <figure id="ico15">
+			      <a href="" alt="" title="">
+			      	<div class="box-shadow ico15">
+			    		<img class="imagem_resort" src="<?php echo base_url('assets');?>/img/semimagem.jpg">
+			    		<img src="<?php echo base_url('assets');?>/img/sobra_box_resort.png">
+			    		<p class="tt_box-shadow">Churrasqueiras</p>
+			    		<p class="txt_box-shadow">Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica.</p>
+	                    <img class="dt_box" src="<?php echo base_url('assets');?>/img/detalhe_sobra_box_resort.png">
+		    	    </div>
+			      	<img src="<?php echo base_url('assets');?>/img/icon_resort.png">
+			      </a>
+		        </figure>
+
+		        <figure id="ico16">
+			      <a href="" alt="" title="">
+			      	<div class="box-shadow ico16">
+			    		<img class="imagem_resort" src="<?php echo base_url('assets');?>/img/semimagem.jpg">
+			    		<img src="<?php echo base_url('assets');?>/img/sobra_box_resort.png">
+			    		<p class="tt_box-shadow">Golf</p>
+			    		<p class="txt_box-shadow">Com drive ranger e put green, um item de lazer que não poderia faltar em Solaris.</p>
+	                    <img class="dt_box" src="<?php echo base_url('assets');?>/img/detalhe_sobra_box_resort.png">
+		    	    </div>
+			      	<img src="<?php echo base_url('assets');?>/img/icon_resort.png">
+			      </a>
+		        </figure>
+
+		        <figure id="ico17">
+			      <a href="" alt="" title="">
+			      	<div class="box-shadow ico17">
+			    		<img class="imagem_resort" src="<?php echo base_url('assets');?>/img/semimagem.jpg">
+			    		<img src="<?php echo base_url('assets');?>/img/sobra_box_resort.png">
+			    		<p class="tt_box-shadow">pista de corrida</p>
+			    		<p class="txt_box-shadow">Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica.</p>
+	                    <img class="dt_box" src="<?php echo base_url('assets');?>/img/detalhe_sobra_box_resort.png">
+		    	    </div>
+			      	<img src="<?php echo base_url('assets');?>/img/icon_resort.png">
+			      </a>
+		        </figure>
+		        
+		        <figure id="ico18">
+			      <a href="" alt="" title="">
+			      	<div class="box-shadow ico18">
+			    		<img class="imagem_resort" src="<?php echo base_url('assets');?>/img/semimagem.jpg">
+			    		<img src="<?php echo base_url('assets');?>/img/sobra_box_resort.png">
+			    		<p class="tt_box-shadow">Salão de jogos e festas</p>
+			    		<p class="txt_box-shadow">Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica.</p>
+	                    <img class="dt_box" src="<?php echo base_url('assets');?>/img/detalhe_sobra_box_resort.png">
+		    	    </div>
+			      	<img src="<?php echo base_url('assets');?>/img/icon_resort.png">
+			      </a>
+		        </figure>
+
+		        <figure id="ico19">
+			      <a href="" alt="" title="">
+			      	<div class="box-shadow ico19">
+			    		<img class="imagem_resort" src="<?php echo base_url('assets');?>/img/semimagem.jpg">
+			    		<img src="<?php echo base_url('assets');?>/img/sobra_box_resort.png">
+			    		<p class="tt_box-shadow">pousada</p>
+			    		<p class="txt_box-shadow">Antes e depois de aproveitar seus dias, nada melhor do que descansar em casa.</p>
 	                    <img class="dt_box" src="<?php echo base_url('assets');?>/img/detalhe_sobra_box_resort.png">
 		    	    </div>
 			      	<img src="<?php echo base_url('assets');?>/img/icon_resort.png">
