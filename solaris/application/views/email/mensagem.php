3aa7cd40c4f5fe9eb20ae5f4f46c3a2a5da01fd7
celsodavid <celunico43@gmail.com>
2014-01-27 11:33:22 -0200

    Celso

--- solaris/application/views/email/mensagem.php
+++ solaris/application/views/email/mensagem.php
@@ -1,8 +1,8 @@
 <?php echo doctype('xhtml1-trans'); ?>
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
-		<meta content="pt-br" http-equiv="Content-Language">
-		<meta content="text/html;charset=utf-8" http-equiv="Content-Type">
+		<meta content="pt-br" http-equiv="Content-Language" />
+		<meta content="text/html;charset=utf-8" http-equiv="Content-Type" />
 		<title>Mensagem de TESTE encaminhada pelo website</title>
 		<style type="text/css">
 			div { width: 700px; float: left; border-bottom: solid 20px #000; padding-bottom: 20px;}
@@ -12,19 +12,35 @@
 	
 	<body>
 		<div>
-		   <img src="<?php echo base_url('assets');?>/img/email.jpg">
-		  <p><strong>Nome:</strong> Aline Albrecht da Silva</p>
-		  <p><strong>E-mail:</strong> alinealbrecht@hotmail.com</p>
-		  <p><strong>Telefone:</strong> (11) 98557-7803</p>
-		  <p><strong>Estado civil:</strong> solteira</p>
-		  <p><strong>Idade:</strong> 27</p>
-		  <p><strong>Filhos:</strong> 0</p>
+		  <img src="<?php echo base_url('assets/img/email.jpg');?>" alt="Solaris Resort Apartamentos em Boituva" />
+		  <p><strong>Nome:</strong> <?php echo $nome;?></p>
+		  <p><strong>E-mail:</strong> <?php $email;?></p>
+		  <p><strong>Telefone:</strong> <?php echo $telefone;?></p>
+		  <p><strong>Estado civil:</strong> <?php echo $estado_civil;?></p>
+		  <p><strong>Idade:</strong> <?php echo $idade;?></p>
+		  <p><strong>Filhos:</strong> <?php echo $filhos;?></p>
 		  <br/>
-		  <p><strong>Conhece o SOLARIS - RESIDENCIAL E RESORT:</strong> Sim</p>
-          <p><strong>Como ficou sabendo sobre o empreendimento:</strong> Internet; Folheto; 
-          Indicação</p>
-          <br/>
-          <p><strong>Mensagem:</strong> Gostaria de saber o preço do apartamento com 47m²</p>
+		  <p><strong>Conhece o SOLARIS - RESIDENCIAL E RESORT:</strong> <?php echo $opcao1;?></p>
+          <p>
+          	<strong>Como ficou sabendo sobre o empreendimento:</strong>
+          	<?php 
+	    	$opcoes = null;
+	    	$i = 0;
+	    	foreach ($items as $item) {
+	    		if(($i % 2) == 0) {
+	    			$opcoes .= $item . '; ';
+	    		}
+	    		else {
+	    			$opcoes .= $item;
+	    			$i++;
+	    		}
+	    	}
+	    	#echo substr($opcoes, 0, -1);
+	    	echo $opcoes;
+	    	?>
+          </p>
+          <br>
+          <p><strong>Mensagem:</strong> <?php echo $mensagem;?></p>
 		</div>
 	</body>
 </html>
