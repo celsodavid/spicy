2363e65a8b2c3b1ac10df022d793f974402ef80a
celsodavid <celunico43@gmail.com>
2014-02-12 15:21:56 -0200

    Celso criação do projeto itaporã

---
+++ itapora/application/config/email.php
@@ -0,0 +1,13 @@
+<?php
+if(!defined('BASEPATH')) exit('No direct script access allowed');
+$config['charset'] = 'utf-8';
+$config['mailtype'] = 'html';
+$config['protocol'] = 'smtp';
+$config['smtp_host'] = '';
+$config['smtp_port'] = 587;
+$config['smtp_timeout'] = 30;
+$config['smtp_user'] = '';
+$config['smtp_pass'] = '';
+$config['charset'] = 'utf-8';
+$config['wordwrap'] = TRUE;
+$config['newline'] = '\r\n';
---
+++ itapora/application/controllers/site.php
@@ -0,0 +1,13 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Site extends CI_Controller {
+
+	public function site()
+	{
+		die('Bem vindo a itapora!');
+		#$this->load->view('');
+	}
+}
+
+/* End of file site.php */
+/* Location: ./application/controllers/site.php */
